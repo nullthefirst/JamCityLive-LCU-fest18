9c27a638fc182ffa54ce3cd37c2e0d9480209ef7
Usheninte <jrandangana@gmail.com>
2018-09-21 14:58:41 +0100

    new build

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 import Intro from './components/intro';
-import Timeline from './components/timeline';
-import Contact from './components/contact';
 import './App.css';
 
 class App extends Component {
@@ -9,8 +7,6 @@ class App extends Component {
     return (
       <div>
         <Intro />
-        <Timeline />
-        <Contact />
       </div>
     );
   }
--- src/components/intro.js
+++ src/components/intro.js
@@ -5,10 +5,7 @@ class Intro extends Component {
   render() {
     return (
       <div>
-        <h1 className="title">LCUJCL Festival 2018</h1>
-        <p>Welcome to the Events log for the Lead City University 
-          <br />
-          JamCityLive Festival 2018</p>
+        <h1>Hello World</h1>
       </div>
     );
   }
