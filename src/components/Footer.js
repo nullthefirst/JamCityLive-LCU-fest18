e50c6a6c5bb9f873b0220eadd355e32df26ab728
Usheninte <jrandangana@gmail.com>
2018-09-21 18:47:42 +0100

    new build

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,14 +5,18 @@ import './Dark.css';
 const footerStyle = {
   textAlign: "center",
   marginTop: "2em"
-}
+};
+
+const linkDark = {
+  color: "white"
+};
 
 export default class Footer extends Component {
   render() {
     return(
       <div style={footerStyle}>
         <h5>JamCityLive Entertainment &copy; 2018 | All Rights Reserved</h5>
-        <h6><a href="https://twitter.com/Usheninte">Coded by Usheninte</a></h6>
+        <h6><a style={linkDark} href="https://twitter.com/Usheninte">Coded by Usheninte</a></h6>
       </div>
     );
   }
