cc37f4e3d478cbd99952ae1f08e18dfc37fb2aa9
Usheninte <jrandangana@gmail.com>
2018-09-22 13:15:52 +0100

    moja

--- src/components/views/events.js
+++ src/components/views/events.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
+import { Table } from 'react-bootstrap';
 import '../Dark.css'
 
 export default class Events extends Component {
   render() {
     return (
       <div>
-        <h1>Hello Events</h1>
+        <h1>Events</h1>
+        <Table striped bordered condensed hover responsive>
+          <thead>
+            <th></th>
+          </thead>
+          <tbody></tbody>
+        </Table>
       </div>
     );
   }
