8acc9051102097a3386a3c74b91f45e68f71bb59
Usheninte <jrandangana@gmail.com>
2018-09-27 19:09:17 +0100

    edited Goals & History

--- src/components/views/435456778899.js
+++
@@ -1,4 +0,0 @@
-        // Goals
-        <Image className="pic" src={'https://res.cloudinary.com/poetrique/image/upload/v1538031478/clients/jamcitylive/iFoundMyInterest.jpg'} title="iFoundMyInterest" alt="iFoundMyInterest" width="350" height="450" />
-        // History
-        <Image className="pic" src={'https://res.cloudinary.com/poetrique/image/upload/v1538031523/clients/jamcitylive/FridayNightSpice.jpg'} title="FridayNightSpice" alt="FridayNightSpice" width="350" height="450" />
