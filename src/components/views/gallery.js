63fcf232099d66f31e30ef6f4c44b17b75ab5b17
Usheninte <jrandangana@gmail.com>
2018-10-29 08:17:45 +0100

    updated Gallery

--- src/components/views/gallery.js
+++ src/components/views/gallery.js
@@ -12,14 +12,19 @@ export default class Gallery extends Component {
       <div>
         <Grid>
           <Row>
-            <Col xs={12} md={4}>
-              <Image className="pic" src={''} alt="promo" />
+            <Col xs={12} md={6}>
+              <Image className="pic" src={'https://res.cloudinary.com/poetrique/image/upload/v1540797146/clients/jamcitylive/fest3.jpg'} alt="promo" />
             </Col>
-            <Col xs={12} md={4}>
-              <Image className="pic" src={''} alt="promo" />
+            <Col xs={12} md={6}>
+              <Image className="pic" src={'https://res.cloudinary.com/poetrique/image/upload/v1540797144/clients/jamcitylive/fest4.jpg'} alt="promo" />
             </Col>
-            <Col xs={12} md={4}>
-              <Image className="pic" src={''} alt="promo" />
+          </Row>
+          <Row>
+            <Col xs={12} md={6}>
+              <Image className="pic" src={'https://res.cloudinary.com/poetrique/image/upload/v1540797215/clients/jamcitylive/fest5.jpg'} alt="promo" />
+            </Col>
+            <Col xs={12} md={6}>
+              <Image className="pic" src={'https://res.cloudinary.com/poetrique/image/upload/v1540797219/clients/jamcitylive/fest6.jpg'} alt="promo" />
             </Col>
           </Row>
         </Grid>
