ee7398d1020289e6b524c8005e69dc534dd4efaf
Usheninte <jrandangana@gmail.com>
2018-10-29 08:08:12 +0100

    edited gallery

--- src/components/views/gallery.js
+++ src/components/views/gallery.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+import { Image } from 'react-bootstrap';
+import { Grid } from 'react-bootstrap';
+import { Row } from 'react-bootstrap';
+import { Col } from 'react-bootstrap';
 
 import '../Dark.css';
 
@@ -6,7 +10,19 @@ export default class Gallery extends Component {
   render() {
     return(
       <div>
-        <p>Hello World</p>
+        <Grid>
+          <Row>
+            <Col xs={12} md={4}>
+              <Image className="pic" src={''} alt="promo" />
+            </Col>
+            <Col xs={12} md={4}>
+              <Image className="pic" src={''} alt="promo" />
+            </Col>
+            <Col xs={12} md={4}>
+              <Image className="pic" src={''} alt="promo" />
+            </Col>
+          </Row>
+        </Grid>
       </div>
     );
   }
