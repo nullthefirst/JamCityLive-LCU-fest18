fabdd1e9b77a431f57cbf81eead1394d65a78ca2
nullthefirst <jrandangana@gmail.com>
2019-01-01 16:01:47 +0100

    new year

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -15,7 +15,7 @@ export default class Footer extends Component {
   render() {
     return(
       <div style={footerStyle}>
-        <h5>JamCityLive Entertainment &copy; 2018 | All Rights Reserved</h5>
+        <h5>JamCityLive Entertainment &copy; 2018 - 2019 | All Rights Reserved</h5>
         <h6><a style={linkDark} href="https://twitter.com/Usheninte">Coded by Usheninte</a></h6>
       </div>
     );
